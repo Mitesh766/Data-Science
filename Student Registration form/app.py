b6389be842af18ce66d7b71d9cf3ab091438726b
Mitesh Agrawal <miteshagrawal972@gmail.com>
2024-04-02 09:43:43 +0530

    credential updated

--- Student Registration form/app.py
+++ Student Registration form/app.py
@@ -9,7 +9,7 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='22Ma33aM@123',
+        password='Pwioi@2301010068',
         port=3307,
         database='registration_db'
     )
